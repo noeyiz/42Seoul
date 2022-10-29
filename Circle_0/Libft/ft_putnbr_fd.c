a32480ab054e94d0c22808d4a9dab9fc2151493f
noeyiz <lamoureclatant@gmail.com>
2022-10-29 13:15:46 +0900

    Libft commit

---
+++ Circle_0/Libft/ft_putnbr_fd.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jikoo <jikoo@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/22 15:29:47 by jikoo             #+#    #+#             */
+/*   Updated: 2022/07/25 16:09:28 by jikoo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long long	nbr;
+
+	if (fd < 0)
+		return ;
+	nbr = (long long)n;
+	if (nbr < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr = -nbr;
+	}
+	if (nbr >= 10)
+	{
+		ft_putnbr_fd(nbr / 10, fd);
+		ft_putnbr_fd(nbr % 10, fd);
+	}
+	else
+		ft_putchar_fd('0' + nbr, fd);
+}
