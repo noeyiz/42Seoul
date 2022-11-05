a4f73b8e0fd83f5acea815d8293fe4e93b2ee0d0
noeyiz <lamoureclatant@gmail.com>
2022-11-05 14:06:49 +0900

    so_long mandatory

---
+++ Circle_2/so_long/mandatory/main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jikoo <jikoo@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/05 13:56:45 by jikoo             #+#    #+#             */
+/*   Updated: 2022/11/05 13:56:58 by jikoo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+void	ft_print_err_and_exit(char *message)
+{
+	printf(RED "Error\n" RESET "%s\n", message);
+	exit(1);
+}
+
+int	main(int argc, char *argv[])
+{
+	t_game	game;
+
+	if (argc != 2)
+		ft_print_err_and_exit("Invalid number of arguments!");
+	ft_init_map(&game.map, argv[1]);
+	ft_start_game(&game);
+	return (0);
+}
---
+++ Circle_2/so_long/mandatory/move.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jikoo <jikoo@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/05 13:58:07 by jikoo             #+#    #+#             */
+/*   Updated: 2022/11/05 13:58:19 by jikoo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+static void	ft_get_cur_idx(t_map *map, int *cur)
+{
+	int	idx;
+
+	idx = 0;
+	while (map->map_str[idx])
+	{
+		if (map->map_str[idx] == 'P')
+			*cur = idx;
+		idx++;
+	}
+}
+
+static void	ft_get_nxt_idx(t_map *map, t_direction direction, int cur, int *nxt)
+{
+	if (direction == Up)
+		*nxt = cur - map->col;
+	else if (direction == Down)
+		*nxt = cur + map->col;
+	else if (direction == Left)
+		*nxt = cur - 1;
+	else if (direction == Right)
+		*nxt = cur + 1;
+}
+
+void	ft_move(t_game *game, t_direction direction)
+{
+	int	cur;
+	int	nxt;
+
+	ft_get_cur_idx(&game->map, &cur);
+	ft_get_nxt_idx(&game->map, direction, cur, &nxt);
+	if (game->map.map_str[nxt] != '1')
+	{
+		if (game->map.map_str[nxt] == 'C')
+			game->collectible--;
+		if (game->map.map_str[nxt] == 'E')
+		{
+			if (game->collectible)
+				return ;
+			else
+				ft_exit_game(game);
+		}
+		printf("count : %d\n", ++game->cnt);
+		game->map.map_str[cur] = '0';
+		game->map.map_str[nxt] = 'P';
+	}
+	ft_set_sprites(game);
+}
