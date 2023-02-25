d820141bc772aed98c24581cc2985638210200ac
noeyiz <lamoureclatant@gmail.com>
2023-02-25 14:37:37 +0900

    Philosophers: mandatory start ~

---
+++ Circle_3/Philosophers/philo/philo.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jikoo <jikoo@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 15:58:36 by jikoo             #+#    #+#             */
+/*   Updated: 2023/02/23 16:18:57 by jikoo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+/* pthread_create, pthread_detach, pthread_join, pthread_mutex_init, 
+   pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock */
+# include <pthread.h>
+/* printf */
+# include <stdio.h>
+/* malloc, free */
+# include <stdlib.h>
+/* memset */
+# include <string.h>
+/* gettimeofday */
+# include <sys/time.h>
+/* write, usleep */
+# include <unistd.h>
+
+#endif
