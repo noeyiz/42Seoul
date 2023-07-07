34a54031cb519b1bb1c962239dbe672f9fbed1b2
noeyiz <lamoureclatant@gmail.com>
2023-07-07 16:47:14 +0900

    CPPModule01: fix sed !

--- Circle_4/CPPModule01/ex04/main.cpp
+++ Circle_4/CPPModule01/ex04/main.cpp
@@ -1,44 +1,48 @@
 #include <fstream>
 #include <iostream>
 
-static int replaceString(const std::string& filename, const std::string& s1, const std::string& s2) {
-    std::ifstream ifs(filename);
-    if (ifs.fail()) {
-        std::cerr << "Failed to open file: " << filename << std::endl;
-        return 1;
-    }
-
-    std::ofstream ofs(filename + ".replace");
-    if (ofs.fail()) {
-        std::cerr << "Failed to create output file." << std::endl;
-        ifs.close();
-        return 1;
-    }
-
-    std::string input;
-    int index;
-
-    while (getline(ifs, input)) {
-        while (!s1.empty() && input.find(s1) != std::string::npos) {
-            index = input.find(s1);
-            ofs << input.substr(0, index) << s2;
-            input = input.substr(index + s1.size());
-        }
-        ofs << input;
-        if (ifs.eof() == false) ofs << std::endl;
-    }
-
-    ifs.close();
-    ofs.close();
-
-    return 0;
-}
-
 int main(int argc, char **argv) {
     if (argc != 4) {
         std::cerr << "Usage: " << argv[0] << " <filename> <string_to_replace> <replacement_string>" << std::endl;
         return 1;
     }
 
-    return replaceString(argv[1], argv[2], argv[3]);
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    std::ifstream ifs(filename);
+    if (ifs.fail()) {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+        return 1;
+    }
+
+    std::string contents;
+    std::string line;
+
+    while (getline(ifs, line)) {
+        contents += line;
+        if (!ifs.eof()) contents += "\n";
+    }
+
+    ifs.close();
+
+    std::ofstream ofs(filename + ".replace");
+    if (ofs.fail()) {
+        std::cerr << "Failed to create output file." << std::endl;
+        return 1;
+    }
+
+    size_t pos;
+    while (!s1.empty() && contents.find(s1) != std::string::npos) {
+        pos = contents.find(s1);
+        ofs << contents.substr(0, pos) << s2;
+        contents = contents.substr(pos + s1.size());
+    }
+    ofs << contents;
+    if (s1.empty()) ofs << s2;
+
+    ofs.close();
+
+    return 0;
 }
