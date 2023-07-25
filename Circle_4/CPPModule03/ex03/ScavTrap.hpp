de58eb2a3be4d191a98de75c2574ba41321c0160
noeyiz <lamoureclatant@gmail.com>
2023-07-25 20:58:52 +0900

    CPPModule03: done

--- Circle_4/CPPModule03/ex03/ScavTrap.hpp
+++ Circle_4/CPPModule03/ex03/ScavTrap.hpp
@@ -3,15 +3,6 @@
 
 # include "ClapTrap.hpp"
 
-# define RESET		"\033[0m"
-# define GRAY		"\033[1;90m"
-# define RED		"\033[1;91m"
-# define GREEN		"\033[1;92m"
-# define YELLOW		"\033[1;93m"
-# define BLUE		"\033[1;94m"
-# define MAGENTA	"\033[1;95m"
-# define CYAN		"\033[1;96m"
-
 class ScavTrap : virtual public ClapTrap {
 public:
     ScavTrap(void);
