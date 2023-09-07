832d2ee004305765bb6ee1d992b7eaa61fb06c88
noeyiz <lamoureclatant@gmail.com>
2023-09-07 20:30:52 +0900

    CPPModule09: ex02

--- Circle_5/CPPModule09/ex02/PmergeMe.cpp
+++ Circle_5/CPPModule09/ex02/PmergeMe.cpp
@@ -1,86 +1,221 @@
 #include "PmergeMe.hpp"
 
 std::deque<int> PmergeMe::deque;
+std::vector<int> PmergeMe::vector;
 
-int getJacobsthalNum(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    return getJacobsthalNum(n - 1) + 2 * getJacobsthalNum(n - 2);
-}
-
-void PmergeMe::parseInput_D(int size, char *input[]) {
+void PmergeMe::parseInput(int size, char **input) {
+    // parse
     int num; char c;
     for (int i = 0; i < size; i++) {
         std::istringstream ss(input[i]);
-        if ((ss >> num) == false || ss >> c
-        || num <= 0 || num != std::atoi(input[i]))
+        if ((ss >> num) == false || ss >> c || num <= 0)
             throw BadInputException();
         deque.push_back(num);
+        vector.push_back(num);
     }
-}
-
-void PmergeMe::checkValid_D() {
-    std::deque<int> temp;
-    for (size_t i = 0; i < deque.size(); i++) temp.push_back(deque[i]);
     // check already sorted
     bool sorted = true;
-    for (size_t i = 0; i < deque.size() - 1; i++) {
-        if (temp[i] > temp[i + 1]) {
+    for (size_t i = 0; i < vector.size() - 1; i++) {
+        if (vector[i] > vector[i + 1]) {
             sorted = false;
             break;
         }
     }
     if (sorted) throw AlreadySortedException();
-    // check duplication
-    std::sort(temp.begin(), temp.end());
-    for (size_t i = 0; i < deque.size() - 1; i++)
-        if (temp[i] == temp[i + 1]) throw DuplicateValueException();
+}
+
+static void insertionSort_V(std::vector<std::pair<int, int> >& elements) {
+    for (size_t i = 1; i < elements.size(); i++) {
+        std::pair<int, int> value = elements[i];
+
+        int j = i - 1;
+        while (j >= 0 && elements[j].second > value.second) {
+            elements[j + 1] = elements[j];
+            j--;
+        }
+        elements[j + 1] = value;
+    }
+}
+
+static void setJacobsthalNumber_V(std::vector<int>& jacobsthal_num, int size) {
+    int prevprev = 1;  // J(n-1)
+    int prev = 3;      // J(n-2)
+    int curr = prev;   // J(n)
+
+    jacobsthal_num.push_back(1);
+    for (int i = 0; i < size - 1; i++) {
+        if (curr-- <= prevprev + 1) {
+            curr = prev + 2 * prevprev; // 점화식 : J(n) = J(n-1) + 2*J(n-2) (n >= 2)
+            if (curr > size) curr = size;
+            prevprev = prev;
+            prev = curr;
+        }
+        jacobsthal_num.push_back(curr);
+    }
+}
+
+static void binarySearchSort_V(std::vector<int>& main_chain, int target, int start, int end) {
+    if (start > end) {
+        main_chain.insert(main_chain.begin() + start, target);
+        return;
+    }
+
+    int mid = start + (end - start) / 2;
+
+    if (main_chain[mid] < target) binarySearchSort_V(main_chain, target, mid + 1, end);
+    else binarySearchSort_V(main_chain, target, start, mid - 1);
+}
+
+void PmergeMe::mergeInsertionSort_V() {
+    // 1. 정렬할 원소의 개수가 홀수일 때 : 마지막 수 (remain) 저장
+    int stray = vector.size() % 2 ? 1 : 0;
+    int remain = stray ? vector.back() : -1;
+
+    // 2. pair <b, a> 만들기 ! 이 때, b < a
+    std::vector<std::pair<int, int> > elements;
+    for (size_t i = 0; i < vector.size() - stray; i += 2) {
+        if (vector[i] < vector[i + 1]) elements.push_back(std::make_pair(vector[i], vector[i + 1]));
+        else elements.push_back(std::make_pair(vector[i + 1], vector[i]));
+    }
+    
+    std::cout << "=== before ===" << std::endl;
+    for (size_t i = 0; i < elements.size(); i++)
+        std::cout << elements[i].first << ", " << elements[i].second << std::endl;
+
+    // 3. a를 기준으로 정렬하기
+    insertionSort_V(elements);
+    // std::sort(elements.begin(), elements.end(), compareSecond);
+
+    std::cout << "=== after ===" << std::endl;
+    for (size_t i = 0; i < elements.size(); i++)
+        std::cout << elements[i].first << ", " << elements[i].second << std::endl;
+
+    // 4. main chain 만들기
+    std::vector<int> main_chain;
+    for (size_t i = 0; i < elements.size(); i++)
+        main_chain.push_back(elements[i].second);
+
+    // 5. jacobsthal number에 따라 pending elements를 main chain 사이에.. 자리 찾아주기
+    std::vector<int> jacobsthal_num;
+    setJacobsthalNumber_V(jacobsthal_num, elements.size());
+    for (size_t i = 0; i < elements.size(); i++) {
+        std::pair<int, int> element = elements[jacobsthal_num[i] - 1];
+        int end = std::find(main_chain.begin(), main_chain.end(), element.second) - main_chain.begin();
+        binarySearchSort_V(main_chain, element.first, 0, end);
+    }
+
+    // 6. remain 있으면 자리 찾아주기
+    if (stray) binarySearchSort_V(main_chain, remain, 0, main_chain.size() - 1);
+
+    // 7. 정렬된 데이터 저장
+    vector.clear();
+    for (size_t i = 0; i < main_chain.size(); i++) vector.push_back(main_chain[i]);
+}
+
+static void insertionSort_D(std::deque<std::pair<int, int> >& elements) {
+    for (size_t i = 1; i < elements.size(); i++) {
+        std::pair<int, int> value = elements[i];
+
+        int j = i - 1;
+        while (j >= 0 && elements[j].second > value.second) {
+            elements[j + 1] = elements[j];
+            j--;
+        }
+        elements[j + 1] = value;
+    }
+}
+
+static void setJacobsthalNumber_D(std::deque<int>& jacobsthal_num, int size) {
+    jacobsthal_num.push_back(1);
+    jacobsthal_num.push_back(3);
+
+    int prevprev = jacobsthal_num[0];
+    int prev = jacobsthal_num[1];
+    int curr = prev;
+
+    for (int i = 0; i < size - 2; i++) {
+        if (curr-- > prevprev + 1) {
+            jacobsthal_num.push_back(curr);
+        } else {
+            curr = prev + 2 * prevprev;
+            if (curr > size) curr = size;
+            jacobsthal_num.push_back(curr);
+            prevprev = prev;
+            prev = curr;
+        }
+    }
+}
+
+static void binarySearchSort_D(std::deque<int>& main_chain, int target, int start, int end) {
+    if (start > end) {
+        main_chain.insert(main_chain.begin() + start, target);
+        return;
+    }
+
+    int mid = start + (end - start) / 2;
+
+    if (main_chain[mid] < target) binarySearchSort_D(main_chain, target, mid + 1, end);
+    else binarySearchSort_D(main_chain, target, start, mid - 1);
 }
 
 void PmergeMe::mergeInsertionSort_D() {
-    // 0. 홀수일 때 마지막 수 저장해두기
     int stray = deque.size() % 2 ? 1 : 0;
     int remain = stray ? deque.back() : -1;
-    // 1. pair <a, b> 만들기 ! 이 때, a > b
-    std::deque<std::pair<int, int> > pair;
+
+    std::deque<std::pair<int, int> > elements;
     for (size_t i = 0; i < deque.size() - stray; i += 2) {
-        std::pair<int, int> p;
-        if (deque[i] > deque[i + 1]) pair.push_back(std::make_pair(deque[i], deque[i + 1]));
-        else pair.push_back(std::make_pair(deque[i + 1], deque[i]));
+        if (deque[i] < deque[i + 1]) elements.push_back(std::make_pair(deque[i], deque[i + 1]));
+        else elements.push_back(std::make_pair(deque[i + 1], deque[i]));
     }
-    std::cout << "=== pair <a, b> 만들기 ===" << std::endl;
-    for (size_t i = 0; i < pair.size(); i++) {
-        std::cout << pair[i].first << ", " << pair[i].second << std::endl;
+    
+    insertionSort_D(elements);
+
+    std::deque<int> main_chain;
+    for (size_t i = 0; i < elements.size(); i++)
+        main_chain.push_back(elements[i].second);
+
+    std::deque<int> jacobsthal_num;
+    setJacobsthalNumber_D(jacobsthal_num, elements.size());
+    for (size_t i = 0; i < elements.size(); i++) {
+        std::pair<int, int> element = elements[jacobsthal_num[i] - 1];
+        int start = 0;
+        int end = std::find(deque.begin(), deque.end(), element.second) - deque.begin();
+        binarySearchSort_D(main_chain, element.first, start, end);
     }
-    // 2. a를 기준으로 정렬하기
-    std::sort(pair.begin(), pair.end());
-    std::cout << "=== a를 기준으로 정렬하기 ===" << std::endl;
-    for (size_t i = 0; i < pair.size(); i++) {
-        std::cout << pair[i].first << ", " << pair[i].second << std::endl;
-    }
-    // 3. main chain, pending elements 배열 만들기
-    std::deque<int> main_chain, pending_elements;
-    for (size_t i = 0; i < pair.size(); i++) {
-        main_chain.push_back(pair[i].first);
-        pending_elements.push_back(pair[i].second);
-    }
-    std::cout << "=== main chain ===" << std::endl;
-    for (size_t i = 0; i < main_chain.size(); i++) {
-        std::cout << main_chain[i] << " ";
-    }
-    std::cout << "\n=== pending elements ===" << std::endl;
-    for (size_t i = 0; i < pending_elements.size(); i++) {
-        std::cout << pending_elements[i] << " ";
-    }
-    if (stray) std::cout << "\n=== remain ===\n" << remain << std::endl;
+
+    if (stray) binarySearchSort_D(main_chain, remain, 0, main_chain.size() - 1);
+
+    deque.clear();
+    for (size_t i = 0; i < main_chain.size(); i++) deque.push_back(main_chain[i]);
 }
 
-void PmergeMe::sort(int size, char *input[]) {
+void PmergeMe::sort(int size, char **input) {
     try {
-        parseInput_D(size, input);
-        checkValid_D();
-        mergeInsertionSort_D();
+        parseInput(size, input);
+        
+        std::cout << "Before: ";
+        for (size_t i = 0; i < vector.size(); i++) std::cout << vector[i] << " ";
         std::cout << std::endl;
+
+        clock_t start_time, end_time;
+        double deque_time, vector_time;
+        
+        start_time = clock();
+        mergeInsertionSort_D();
+        end_time = clock();
+        deque_time = static_cast<double>(end_time - start_time);
+        
+        start_time = clock();
+        mergeInsertionSort_V();
+        end_time = clock();
+        vector_time = static_cast<double>(end_time - start_time);
+        
+        std::cout << "After: ";
+        for (size_t i = 0; i < vector.size(); i++) std::cout << vector[i] << " ";
+        std::cout << std::endl;
+
+        std::cout << "Time to process a range of " << size << " elements with std::deque : " << deque_time << "ms" << std::endl;
+        std::cout << "Time to process a range of " << size << " elements with std::vector : " << vector_time << "ms" << std::endl;
     } catch (std::exception& e) {
         std::cout << "Error: " << e.what() << std::endl;
     }
@@ -92,8 +227,4 @@ const char* PmergeMe::AlreadySortedException::what() const throw() {
 
 const char* PmergeMe::BadInputException::what() const throw() {
     return "bad input";
-}
-
-const char* PmergeMe::DuplicateValueException::what() const throw() {
-    return "duplicate value";
 }
--- Circle_5/CPPModule09/ex02/PmergeMe.hpp
+++ Circle_5/CPPModule09/ex02/PmergeMe.hpp
@@ -2,28 +2,35 @@
 # define PMERGEME_HPP
 
 # include <algorithm>
+# include <ctime>
 # include <deque>
 # include <iostream>
 # include <sstream>
 # include <string>
+# include <utility>
 # include <vector>
+
+# define RESET		"\033[0m"
+# define GRAY		"\033[1;90m"
+# define RED		"\033[1;91m"
 
 class PmergeMe {
 private:
     static std::deque<int> deque;
-    // static std::vector<int> vector;
+    static std::vector<int> vector;
 
     PmergeMe();
     PmergeMe(const PmergeMe& other);
     PmergeMe& operator=(const PmergeMe& other);
     ~PmergeMe();
 
-    static void parseInput_D(int size, char *input[]);
-    static void checkValid_D();
+    static void parseInput(int size, char **input);
     static void mergeInsertionSort_D();
+    static void parseInput_V(int size, char **input);
+    static void mergeInsertionSort_V();
 
 public:
-    static void sort(int size, char *input[]);
+    static void sort(int size, char **input);
 
     class AlreadySortedException : public std::exception {
     public:
@@ -31,11 +38,6 @@ public:
     };
 
     class BadInputException : public std::exception {
-    public:
-        virtual const char* what() const throw();
-    };
-
-    class DuplicateValueException : public std::exception {
     public:
         virtual const char* what() const throw();
     };
