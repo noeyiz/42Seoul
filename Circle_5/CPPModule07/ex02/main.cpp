9112dfa87de913637adf6e155f255c8c9042f100
noeyiz <lamoureclatant@gmail.com>
2023-09-09 14:11:34 +0900

    CPPModule07: fix main.cpp

--- Circle_5/CPPModule07/ex02/main.cpp
+++ Circle_5/CPPModule07/ex02/main.cpp
@@ -9,7 +9,7 @@ int main() {
     size_t size = 5;
     try {
         // 예시 1) int 배열
-        Array<int> int_arr(5);
+        Array<int> int_arr(size);
         for (size_t i = 0; i < int_arr.size(); i++)
             int_arr[i] = i;
         
@@ -18,7 +18,7 @@ int main() {
         std::cout << std::endl;
 
         // 예시 2) char 배열
-        Array<char> char_arr(5);
+        Array<char> char_arr(size);
         for (size_t i = 0; i < char_arr.size(); i++)
             char_arr[i] = static_cast<char>('a' + i);
 
